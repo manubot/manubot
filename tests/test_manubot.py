e7c88efaaf73747b3990b548cfce4351060720e0
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-11-29 11:11:23 -0500

    Test for read_jsons

--- tests/test_manubot.py
+++ tests/test_manubot.py
@@ -3,6 +3,8 @@ import subprocess
 
 import pytest
 
+from manubot.manubot import read_jsons
+
 directory = pathlib.Path(__file__).parent.resolve()
 
 # List of manuscripts for testing. All subdirectories of ./manuscripts
@@ -35,3 +37,27 @@ def test_example_manuscript(manuscript):
     print(process.args)
     print(process.stderr.decode())
     assert process.returncode == 0
+
+
+def test_read_jsons():
+    """
+    Test reading multiple JSON files, from both local paths and URLs.
+    """
+    local_path = 'manuscripts/variables/content/template-variables.json'
+    local_path = directory.joinpath(local_path)
+    paths = [
+        'https://git.io/vbkqm',
+        'https://git.io/vbkqm',
+        'namespace_1:https://git.io/vbkqm',
+        'namespace_2:https://git.io/vbkqm',
+        f'namespace_2:{local_path}',
+        f'namespace_3:{local_path}',
+    ]
+    user_variables = read_jsons(paths)
+    assert 'namespace_1' in user_variables
+    assert 'namespace_2' in user_variables
+    assert 'namespace_3' in user_variables
+    assert user_variables['generated_by'] == 'Manubot'
+    assert 'violet' in user_variables['namespace_1']['rainbow']
+    assert 'yellow' in user_variables['namespace_2']['rainbow']
+    assert 'orange' in user_variables['namespace_3']['rainbow']
