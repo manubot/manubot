9916977f0154a5590eccdc824c52f23f7722e1e0
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2018-01-08 12:22:31 -0500

    Add tests for author affiliation numbering

--- tests/test_manubot.py
+++ tests/test_manubot.py
@@ -3,7 +3,10 @@ import subprocess
 
 import pytest
 
-from manubot.manubot import read_jsons
+from manubot.manubot import (
+    add_author_affiliations,
+    read_jsons,
+)
 
 directory = pathlib.Path(__file__).parent.resolve()
 
@@ -67,3 +70,36 @@ def test_read_jsons():
     assert 'violet' in user_variables['namespace_1']['rainbow']
     assert 'yellow' in user_variables['namespace_2']['rainbow']
     assert 'orange' in user_variables['namespace_3']['rainbow']
+
+
+def test_add_author_affiliations_empty():
+    variables = {}
+    variables['authors'] = [
+        {'name': 'Jane Roe'},
+        {'name': 'John Doe'},
+    ]
+    returned_variables = add_author_affiliations(variables)
+    assert variables is returned_variables
+    assert 'affiliations' not in variables
+    for author in variables['authors']:
+        assert 'affiliation_numbers' not in author
+
+
+def test_add_author_affiliations():
+    variables = {}
+    variables['authors'] = [
+        # Legacy affiliations format (as a string that's `: ` separated)
+        {'name': 'Jane Roe', 'affiliations': 'Department of Doe, University of Roe; Peppertea University'},
+        # Prefered affiliations format as a list
+        {'name': 'John Doe', 'affiliations': ['Unique University', 'Peppertea University']},
+    ]
+    returned_variables = add_author_affiliations(variables)
+    assert variables is returned_variables
+    assert variables['affiliations'] == [
+        {'affiliation': 'Department of Doe, University of Roe', 'affiliation_number': 1},
+        {'affiliation': 'Peppertea University', 'affiliation_number': 2},
+        {'affiliation': 'Unique University', 'affiliation_number': 3},
+    ]
+    authors = variables['authors']
+    assert authors[0]['affiliation_numbers'] == [1, 2]
+    assert authors[1]['affiliation_numbers'] == [3, 2]
