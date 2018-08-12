f638c6ab413775e4d2bf1b8f35ef2fc8e7fb29d0
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2018-08-12 18:05:56 -0400

    Simplify output

--- tests/test_command.py
+++ tests/test_command.py
@@ -4,16 +4,12 @@ import manubot
 
 
 def test_version():
-    process = subprocess.run(
+    stdout = subprocess.check_output(
         ['manubot', '--version'],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
         universal_newlines=True,
     )
-    print(process.stderr)
-    assert process.returncode == 0
     version_str = f'v{manubot.__version__}'
-    assert version_str == process.stdout.strip()
+    assert version_str == stdout.rstrip()
 
 
 def test_missing_subcommand():
