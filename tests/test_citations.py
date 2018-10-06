78c014f5eddcefc0ab50eb34dcfec8ee4ad3cc6d
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2018-10-06 10:06:22 -0400

    Update CSL style commit hash
    Includes block spacing from
    https://github.com/greenelab/manubot-rootstock/pull/134

--- tests/test_citations.py
+++ tests/test_citations.py
@@ -258,21 +258,21 @@ DOI: 10.1098/rsif.2017.0387 · PMID: 29618526 · PMCID: PMC5938574
 '''
 
 references_markdown = '''\
-1. **Generalization of the Fermi Pseudopotential**  
-Trang T. Le, Zach Osman, D. K. Watson, Martin Dunn, B. A. McKinney  
-  
+1. **Generalization of the Fermi Pseudopotential**
+Trang T. Le, Zach Osman, D. K. Watson, Martin Dunn, B. A. McKinney
+
 *arXiv* (2018-06-14) <https://arxiv.org/abs/1806.05726v1>
 
-2. **Vagelos Report Summer 2017**  
-Michael Zietz  
-  
-*Figshare* (2017) <https://doi.org/gdz6dd>  
+2. **Vagelos Report Summer 2017**
+Michael Zietz
+
+*Figshare* (2017) <https://doi.org/gdz6dd>
 DOI: [10.6084/m9.figshare.5346577.v1](https://doi.org/10.6084/m9.figshare.5346577.v1)
 
-3. **Opportunities and obstacles for deep learning in biology and medicine.**  
-Travers Ching, Daniel S Himmelstein, Brett K Beaulieu-Jones, Alexandr A Kalinin, Brian T Do, Gregory P Way, Enrico Ferrero, Paul-Michael Agapow, Michael Zietz, Michael M Hoffman, … Casey S Greene  
-  
-*Journal of the Royal Society, Interface* (2018-04) <https://www.ncbi.nlm.nih.gov/pubmed/29618526>  
+3. **Opportunities and obstacles for deep learning in biology and medicine.**
+Travers Ching, Daniel S Himmelstein, Brett K Beaulieu-Jones, Alexandr A Kalinin, Brian T Do, Gregory P Way, Enrico Ferrero, Paul-Michael Agapow, Michael Zietz, Michael M Hoffman, … Casey S Greene
+
+*Journal of the Royal Society, Interface* (2018-04) <https://www.ncbi.nlm.nih.gov/pubmed/29618526>
 DOI: [10.1098/rsif.2017.0387](https://doi.org/10.1098/rsif.2017.0387) · PMID: [29618526](http://www.ncbi.nlm.nih.gov/pubmed/29618526) · PMCID: [PMC5938574](http://www.ncbi.nlm.nih.gov/pmc/articles/PMC5938574)
 '''
 
@@ -421,7 +421,7 @@ def test_cite_command_render_stdout(args, expected):
     """
     args = [
         'manubot', 'cite', '--render',
-        '--csl', 'https://github.com/greenelab/manubot-rootstock/raw/3df864ecda8e22e738bf6f7f24833799b99cd961/build/assets/style.csl',
+        '--csl', 'https://github.com/greenelab/manubot-rootstock/raw/e83e51dcd89256403bb787c3d9a46e4ee8d04a9e/build/assets/style.csl',
         'arxiv:1806.05726v1', 'doi:10.6084/m9.figshare.5346577.v1', 'pmid:29618526',
     ] + args
     process = subprocess.run(
