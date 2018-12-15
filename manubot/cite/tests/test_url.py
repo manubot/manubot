d47c40d5e8be166faf4784ae358eff2ee9f1ecc5
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2018-12-14 22:14:16 -0500

    test_get_url_citeproc_zotero: add curl command to docstring

--- manubot/cite/tests/test_url.py
+++ manubot/cite/tests/test_url.py
@@ -2,6 +2,16 @@ from manubot.cite.url import get_url_citeproc_zotero
 
 
 def test_get_url_citeproc_zotero():
+    """
+    This command creates two translation-server queries, which can be
+    mimmicked via curl:
+    ```
+    curl --verbose \
+      --header "Content-Type: text/plain" \
+      --data 'https://nyti.ms/1NuB0WJ' \
+      'https://translate.manubot.org/web'
+    ```
+    """
     url = 'https://nyti.ms/1NuB0WJ'
     csl_item = get_url_citeproc_zotero(url)
     assert csl_item['title'] == 'Unraveling the Ties of Altitude, Oxygen and Lung Cancer'
