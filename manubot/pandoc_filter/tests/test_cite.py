8c46652c9b75573ff18cbc89ad6718b54f12ef2f
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-03-12 15:18:04 -0400

    Fix tests

--- manubot/pandoc_filter/tests/test_cite.py
+++ manubot/pandoc_filter/tests/test_cite.py
@@ -23,17 +23,32 @@ def test_cite_pandoc_filter():
     """
     Test the stdout output of `manubot cite --render` with various formats.
     The output is sensitive to the version of Pandoc used, so rather than fail when
-    the system's pandoc is outdated, the test is skipped. 
+    the system's pandoc is outdated, the test is skipped.
+
+    ```
+    # Command to rebuild the expected output
+    pandoc \
+    --to=plain \
+    --wrap=preserve \
+    --csl=https://github.com/manubot/rootstock/raw/af1d47a0ec5f33d8fc99deab2ac23b697983b673/build/assets/style.csl \
+    --output=manubot/pandoc_filter/tests/output-with-cites.txt \
+    --filter=pandoc-manubot-cite \
+    --filter=pandoc-citeproc \
+    manubot/pandoc_filter/tests/input-with-cites.md
+    ```
     """
     pandoc_version = _get_pandoc_info()['pandoc version']
     if pandoc_version < (1, 12):
         pytest.skip("Test requires pandoc >= 1.12 to support --filter")
     input_md = directory.joinpath('input-with-cites.md').read_text()
-    expected = directory.joinpath('output-with-cites.md').read_text()
+    expected = directory.joinpath('output-with-cites.txt').read_text()
     args = [
         'pandoc',
+        '--wrap', 'preserve',
+        '--csl', 'https://github.com/manubot/rootstock/raw/af1d47a0ec5f33d8fc99deab2ac23b697983b673/build/assets/style.csl',
         '--filter', 'pandoc-manubot-cite',
-        '--to', 'markdown',
+        '--filter', 'pandoc-citeproc',
+        '--to', 'plain',
     ]
     process = subprocess.run(
         args,
