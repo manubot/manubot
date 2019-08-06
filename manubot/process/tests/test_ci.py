b1d16294ebe08b29b6da489ce7c539df5e791942
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-08-06 16:04:12 -0400

    Fix tests

--- manubot/process/tests/test_ci.py
+++ manubot/process/tests/test_ci.py
@@ -10,22 +10,24 @@ def test_get_continuous_integration_parameters_travis():
     info = get_continuous_integration_parameters()
     assert info is not None
     assert info['provider'] == 'travis'
-    assert info['repo_slug'] == 'manubot/rootstock'
+    assert info['repo_slug'] == 'manubot/manubot'
     assert info['repo_owner'] == 'manubot'
+    assert info['repo_name'] == 'manubot'
     assert info['commit']
     assert info['triggering_commit']
     assert info['build_url'].startswith('https://travis-ci.com/manubot/manubot/builds/')
     assert info['job_url'].startswith('https://travis-ci.com/manubot/manubot/jobs/')
 
 
-@pytest.mark.skipif('TRAVIS' not in os.environ, reason='tests environment variables set by AppVeyor builds only')
+@pytest.mark.skipif('APPVEYOR' not in os.environ, reason='tests environment variables set by AppVeyor builds only')
 def test_get_continuous_integration_parameters_appveyor():
     info = get_continuous_integration_parameters()
     assert info is not None
     assert info['provider'] == 'appveyor'
     assert info['provider_account'] == 'manubot'
-    assert info['repo_slug'] == 'manubot/rootstock'
+    assert info['repo_slug'] == 'manubot/manubot'
     assert info['repo_owner'] == 'manubot'
+    assert info['repo_name'] == 'manubot'
     assert info['commit']
     assert info['triggering_commit']
     assert info['build_url'].startswith('https://ci.appveyor.com/project/manubot/manubot/builds/')
