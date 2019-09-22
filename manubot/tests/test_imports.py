0381ff4242bfc3f0eb9327e39b9b52a030c29320
Evgeniy <e.pogrebnyak@gmail.com>
2019-09-22 14:45:39 +0300

    split test_imports()

--- manubot/tests/test_imports.py
+++ manubot/tests/test_imports.py
@@ -1,10 +1,14 @@
 
 def test_imports():
     import manubot.cite
-    import manubot.cite.util
     import manubot.cite.arxiv
     import manubot.cite.doi
     import manubot.cite.pubmed
     import manubot.cite.url
     import manubot.process.manuscript
-    assert isinstance(manubot.cite.util.citeproc_retrievers, dict)
+
+# FIXME: citeproc_retrievers are assumed to go away when moving 
+#        functionality to Handle dataclass
+def assert_instance_type():
+    from manubot.cite.citekey import citeproc_retrievers
+    assert isinstance(citeproc_retrievers, dict)
