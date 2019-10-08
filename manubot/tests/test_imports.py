ee2e71c2384be07cb35238cf943e910e5fe27cd8
Evgeniy <e.pogrebnyak@gmail.com>
2019-10-08 17:55:57 +0300

    deleted fixme

--- manubot/tests/test_imports.py
+++ manubot/tests/test_imports.py
@@ -7,8 +7,6 @@ def test_imports():
     import manubot.cite.url
     import manubot.process.manuscript
 
-# FIXME: citeproc_retrievers are assumed to go away when moving 
-#        functionality to Handle dataclass
 def assert_instance_type():
     from manubot.cite.citekey import citeproc_retrievers
     assert isinstance(citeproc_retrievers, dict)
