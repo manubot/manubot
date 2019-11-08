9023b7fbfa0b235c14a4d702516bc0cd6d3101ed
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-11-08 13:32:27 -0500

    process: set / detect manuscript thumbnail image (#169)
    merges https://github.com/manubot/manubot/pull/169
    refs https://github.com/manubot/rootstock/issues/267
    
    Support `metadata.thumbnail` for setting the thumbnail.
    Output `variables.thumbnail_url` with the absolute thumbnail URL.
    
    git_repository_root function was derived from _git_project_root in
    https://github.com/pdoc3/pdoc/commit/b47e916d78821dd9918b83f4a23b859b9cc473c5

---
+++ manubot/process/tests/test_thumbnail.py
@@ -0,0 +1,54 @@
+import pytest
+
+
+from ..thumbnail import get_thumbnail_url
+from ..ci import get_continuous_integration_parameters
+
+ci_params = get_continuous_integration_parameters() or {}
+local_only = pytest.mark.skipif(
+    ci_params, reason="skipping on local build since test assumes supported CI behavior"
+)
+ci_only = pytest.mark.skipif(
+    not ci_params, reason="skipping on CI build since test assumes local behavior"
+)
+
+repo_raw_url_template = (
+    f"https://github.com/{ci_params.get('repo_slug', '')}"
+    f"/raw/{ci_params.get('triggering_commit', '')}/"
+)
+example_thumbnail_url = (
+    repo_raw_url_template + "manubot/process/tests/manuscripts/example/thumbnail.png"
+)
+
+
+@pytest.mark.parametrize(
+    ("thumbnail", "expected"),
+    [
+        pytest.param(None, None, id="None-on-local", marks=local_only),
+        pytest.param("", None, id="empty-on-local", marks=local_only),
+        pytest.param(None, example_thumbnail_url, id="None-on-ci", marks=ci_only),
+        pytest.param("", example_thumbnail_url, id="empty-on-ci", marks=ci_only),
+        pytest.param(
+            "path/to/thumbnail.png", None, id="local-path-on-local", marks=local_only
+        ),
+        pytest.param(
+            "path/to/thumbnail.png",
+            repo_raw_url_template + "path/to/thumbnail.png",
+            id="local-path-on-ci",
+            marks=ci_only,
+        ),
+        pytest.param(
+            "http://example.com/thumbnail.png",
+            "http://example.com/thumbnail.png",
+            id="url-http",
+        ),
+        pytest.param(
+            "https://example.com/thumbnail.png",
+            "https://example.com/thumbnail.png",
+            id="url-https",
+        ),
+    ],
+)
+def test_get_thumbnail_url(thumbnail, expected):
+    thumbnail_url = get_thumbnail_url(thumbnail)
+    assert expected == thumbnail_url
