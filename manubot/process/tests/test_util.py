0dca6d2d716d0895bc706aec7a4d49a5d5a475c5
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-11-11 15:48:38 -0500

    do updates python dictionary

--- manubot/process/tests/test_util.py
+++ manubot/process/tests/test_util.py
@@ -1,3 +1,4 @@
+import copy
 import pathlib
 
 import pytest
@@ -87,7 +88,7 @@ def test_get_header_includes_description_abstract():
         "pandoc": {"abstract": "value for abstract"},
         "manubot": {"description": "value for description"},
     }
-    header_includes = get_header_includes(variables)
+    header_includes = get_header_includes(copy.deepcopy(variables))
     assert (
         '<meta name="citation_abstract" content="value for abstract" />'
         in header_includes
@@ -97,7 +98,7 @@ def test_get_header_includes_description_abstract():
     )
     # test that abstract is used for description if description is not defined
     del variables["manubot"]["description"]
-    header_includes = get_header_includes(variables)
+    header_includes = get_header_includes(copy.deepcopy(variables))
     assert (
         '<meta name="citation_abstract" content="value for abstract" />'
         in header_includes
@@ -105,6 +106,6 @@ def test_get_header_includes_description_abstract():
     assert '<meta name="description" content="value for abstract" />' in header_includes
     # test that if neither abstract nor description is defined, neither are inserted
     del variables["pandoc"]["abstract"]
-    header_includes = get_header_includes(variables)
+    header_includes = get_header_includes(copy.deepcopy(variables))
     assert 'meta name="citation_abstract"' not in header_includes
     assert 'meta name="description"' not in header_includes
