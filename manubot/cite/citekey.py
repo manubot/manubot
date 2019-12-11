ad67bfc06a9044b5667e6cbe93f8a7c125200a50
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-12-11 08:44:37 -0700

    match 6+ digits for biorxiv ids

--- manubot/cite/citekey.py
+++ manubot/cite/citekey.py
@@ -324,8 +324,8 @@ def url_to_citekey(url):
         doi = unquote(parsed_url.path.lstrip("/"))
         return f"doi:{doi}"
     if parsed_url.hostname.split(".")[-2:] == ["biorxiv", "org"]:
-        # bioRxiv URL to DOI
-        (biorxiv_id,) = re.findall(r"/([0-9]{6})", parsed_url.path)
+        # bioRxiv URL to DOI. See https://git.io/Je9Hq
+        (biorxiv_id,) = re.findall(r"/([0-9]{6,})", parsed_url.path)
         return f"doi:10.1101/{biorxiv_id}"
     is_ncbi_url = parsed_url.hostname.endswith("ncbi.nlm.nih.gov")
     if is_ncbi_url and parsed_url.path.startswith("/pubmed/"):
