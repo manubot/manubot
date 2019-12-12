38e60f34dea43da51bb01d2b4e61ba462e8876fe
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-12-12 15:08:05 -0500

    Support new biorxiv URLs, inspect identifiers
    arxiv regex derived from regex at
    https://www.wikidata.org/w/index.php?title=Property:P818&oldid=989540062

--- manubot/cite/tests/test_citekey.py
+++ manubot/cite/tests/test_citekey.py
@@ -66,6 +66,11 @@ def test_shorten_citekey(standard_citekey, expected):
         "pmcid:PMC4304851",
         "pmid:25648772",
         "arxiv:1407.3561",
+        "arxiv:1407.3561v1",
+        "arxiv:math.GT/0309136",
+        "arxiv:math.GT/0309136v1",
+        "arxiv:hep-th/9305059",
+        "arxiv:hep-th/9305059v2",
         "isbn:978-1-339-91988-1",
         "isbn:1-339-91988-5",
         "wikidata:Q1",
@@ -95,6 +100,7 @@ def test_inspect_citekey_passes(citekey):
         ("isbn:1-339-91988-X", "identifier violates the ISBN syntax"),
         ("wikidata:P212", "item IDs must start with 'Q'"),
         ("wikidata:QABCD", "does not conform to the Wikidata regex"),
+        ("arxiv:YYMM.number", "must conform to syntax"),
     ],
 )
 def test_inspect_citekey_fails(citekey, contains):
@@ -125,24 +131,67 @@ def test_infer_citekey_prefix(citation, expect):
 @pytest.mark.parametrize(
     ["url", "citekey"],
     [
+        ("https://www.doi.org/", "url:https://www.doi.org/",),
+        (
+            "https://www.doi.org/factsheets/Identifier_Interoper.html",
+            "url:https://www.doi.org/factsheets/Identifier_Interoper.html",
+        ),
         (
             "https://doi.org/10.1097%2F00004032-200403000-00012",
             "doi:10.1097/00004032-200403000-00012",
         ),
         ("http://dx.doi.org/10.7554/eLife.46574", "doi:10.7554/eLife.46574"),
         ("https://doi.org/10/b6vnmd#anchor", "doi:10/b6vnmd"),
+        # ShortDOI URLs without `10/` prefix not yet supported`
+        ("https://doi.org/b6vnmd", "url:https://doi.org/b6vnmd"),
+        (
+            "https://www.biorxiv.org/about-biorxiv",
+            "url:https://www.biorxiv.org/about-biorxiv",
+        ),
         ("https://www.biorxiv.org/content/10.1101/087619v3", "doi:10.1101/087619"),
         ("https://www.biorxiv.org/content/10.1101/087619v3.full", "doi:10.1101/087619"),
         (
             "https://www.biorxiv.org/content/early/2017/08/31/087619.full.pdf",
             "doi:10.1101/087619",
         ),
+        (
+            "https://www.biorxiv.org/content/10.1101/2019.12.11.872580v1",
+            "doi:10.1101/2019.12.11.872580",
+        ),
+        (
+            "https://www.biorxiv.org/content/10.1101/2019.12.11.872580v1.full.pdf+html",
+            "doi:10.1101/2019.12.11.872580",
+        ),
+        (
+            "https://www.biorxiv.org/content/10.1101/2019.12.11.872580v1.full.pdf",
+            "doi:10.1101/2019.12.11.872580",
+        ),
+        ("https://www.ncbi.nlm.nih.gov", "url:https://www.ncbi.nlm.nih.gov"),
+        (
+            "https://www.ncbi.nlm.nih.gov/pubmed",
+            "url:https://www.ncbi.nlm.nih.gov/pubmed",
+        ),
         ("https://www.ncbi.nlm.nih.gov/pubmed/31233491", "pmid:31233491"),
+        ("https://www.ncbi.nlm.nih.gov/pmc/", "url:https://www.ncbi.nlm.nih.gov/pmc/"),
+        (
+            "https://www.ncbi.nlm.nih.gov/pmc/about/intro/",
+            "url:https://www.ncbi.nlm.nih.gov/pmc/about/intro/",
+        ),
         ("https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4304851/", "pmcid:PMC4304851"),
+        (
+            "https://www.wikidata.org/wiki/Wikidata:Main_Page",
+            "url:https://www.wikidata.org/wiki/Wikidata:Main_Page",
+        ),
         ("https://www.wikidata.org/wiki/Q50051684", "wikidata:Q50051684"),
+        ("https://arxiv.org/", "url:https://arxiv.org/"),
+        (
+            "https://arxiv.org/list/q-fin/recent",
+            "url:https://arxiv.org/list/q-fin/recent",
+        ),
         ("https://arxiv.org/abs/1912.03529v1", "arxiv:1912.03529v1"),
         ("https://arxiv.org/pdf/1912.03529v1.pdf", "arxiv:1912.03529v1"),
         ("https://arxiv.org/ps/1912.03529v1", "arxiv:1912.03529v1"),
+        ("https://arxiv.org/abs/math.GT/0309136", "arxiv:math.GT/0309136"),
         ("https://arxiv.org/abs/hep-th/9305059", "arxiv:hep-th/9305059"),
         ("https://arxiv.org/pdf/hep-th/9305059.pdf", "arxiv:hep-th/9305059"),
     ],
