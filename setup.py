e5f47504d8f94ec0bdcd9c973ea39c4d7fe12f09
dhimmel <dhimmel@users.noreply.github.com>
2020-05-22 03:06:40 +0000

    deploy: 20ba7e36b971d62e78d6436f793f7ffac9975eb3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = readme_path.read_text(encoding="utf-8-sig")
 # extra dependencies with an "all" option
 extras_require = {
     "webpage": ["opentimestamps-client"],
-    "dev": ["black", "exrex", "flake8", "ghp-import", "portray", "pytest", "yamllint"],
+    "dev": ["black", "exrex", "flake8", "portray", "pytest", "yamllint"],
 }
 extras_require["all"] = list(
     dict.fromkeys(itertools.chain.from_iterable(extras_require.values()))
