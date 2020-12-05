1efdcb21a8b6e8ed788809021727fb4e15775444
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2020-12-05 13:11:17 -0500

    declarative version

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
-import pathlib
-import re
-
 import setuptools
 
-directory = pathlib.Path(__file__).parent.resolve()
-
-# version
-init_path = directory.joinpath("manubot", "__init__.py")
-text = init_path.read_text(encoding="utf-8-sig")
-pattern = re.compile(r"^__version__ = ['\"]([^'\"]*)['\"]", re.MULTILINE)
-version = pattern.search(text).group(1)
-
-setuptools.setup(version=version)
+if __name__ == "__main__":
+    setuptools.setup()
