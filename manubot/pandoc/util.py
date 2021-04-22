52aa09fbeaf7e1c9da1c7ec66514a825185a6953
Sergey Lazarev <46479545+lasest@users.noreply.github.com>
2021-04-22 16:46:38 +0500

    Check pandoc-citeproc version only if required
    merges https://github.com/manubot/manubot/pull/287
    closes https://github.com/manubot/manubot/issues/286
    
    Checking pandoc-citeproc version only if it is required
    (i.e. for older pandoc versions).

--- manubot/pandoc/util.py
+++ manubot/pandoc/util.py
@@ -30,21 +30,13 @@ def get_pandoc_info() -> Dict[str, Any]:
     }
     ```
     """
-    stats = dict()
-    for command in "pandoc", "pandoc-citeproc":
-        path = shutil.which(command)
-        stats[command] = bool(path)
-        if not path:
-            continue
-        version = subprocess.check_output(args=[command, "--version"], encoding="utf-8")
-        logging.debug(version)
-        version, *_discard = version.splitlines()
-        _discard, version = version.strip().split()
-        from packaging.version import parse as parse_version
 
-        version = parse_version(version).release
-        stats[f"{command} version"] = version
-        stats[f"{command} path"] = path
+    stats = get_command_info("pandoc")
+    if stats["pandoc version"] < (2, 11):
+        stats.update(get_command_info("pandoc-citeproc"))
+    else:
+        stats["pandoc-citeproc"] = False
+
     logging.info("\n".join(f"{k}: {v}" for k, v in stats.items()))
     return stats
 
@@ -59,3 +51,29 @@ def get_pandoc_version() -> Tuple[int, ...]:
         # https://twitter.com/dhimmel/status/1327082301994496000
         raise ImportError("missing pandoc command on system.")
     return pandoc_info["pandoc version"]
+
+
+def get_command_info(command: str) -> dict:
+    """
+    Returns a dictionary containing some information about a command
+    """
+
+    command_info_dict = dict()
+
+    path = shutil.which(command)
+    command_info_dict[command] = bool(path)
+
+    if not path:
+        return command_info_dict
+
+    version = subprocess.check_output(args=[command, "--version"], encoding="utf-8")
+    logging.debug(version)
+    version, *_discard = version.splitlines()
+    _discard, version = version.strip().split()
+    from packaging.version import parse as parse_version
+
+    version = parse_version(version).release
+    command_info_dict[f"{command} version"] = version
+    command_info_dict[f"{command} path"] = path
+
+    return command_info_dict
