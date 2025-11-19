4c1e83897a45be9f1198e64768695775f2e6c379
d33bs <ekgto445@gmail.com>
2025-11-19 12:13:07 -0700

    increase retries

--- manubot/util.py
+++ manubot/util.py
@@ -128,9 +128,9 @@ def request_with_retry(
     method: str = "get",
     params: typing.Optional[dict] = None,
     headers: typing.Optional[dict] = None,
-    retries: int = 2,
+    retries: int = 5,
     retry_statuses: typing.Tuple[int, ...] = (429,),
-    delay: float = 2.0,
+    delay: float = 3.0,
     **kwargs,
 ):
     """
